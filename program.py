41bac7d57a84c4e1983989897b38c46eb0b65cce
Alex Azh <alex.azh5@gmail.com>
2023-04-09 20:46:11 +0500

    worked

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import pickle
 from pathlib import Path
 
 import faiss
@@ -8,6 +9,17 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 
 from FileReaders import PDFReader, DOCReader
+
+
+def SaveArray(arr: list, name: str):
+    with open(name + '.pkl', 'wb') as file:
+        pickle.dump(arr, file)
+
+
+def OpenArray(name: str) -> list:
+    with open(name + '.pkl', 'rb') as file:
+        loaded_array = pickle.load(file)
+    return loaded_array
 
 
 def Message(s: str, beforeTimestamp):
@@ -55,14 +67,13 @@ def mainFunc():
     res = index.search(query_for_search, 3)  # index.ntotal)
     currTime = Message(f"Поиск занял...", currTime)
     print(res[1])
-    text="The analytic continuation is an old yet persistent problem,"
+    text = "The analytic continuation is an old yet persistent problem,"
     currTime = Message(f"Начало поиска 3...", currTime)
-    res = index.search(model.encode([text]),3)
+    res = index.search(model.encode([text]), 3)
     currTime = Message(f"Конец поиска 3...", currTime)
     # readedFiles=[]
     # for i, file in enumerate(os.listdir("texts")):
     #     readedFiles.append(file)
-
 
 
 def GetVectorsByFiles(dir, model: SentenceTransformer, readedFiles: list, nonReadedFiles: list):
@@ -77,13 +88,19 @@ def GetVectorsByFiles(dir, model: SentenceTransformer, readedFiles: list, nonRea
                 yield np.reshape(model.encode(text, convert_to_numpy=True), (1, -1))
                 readedFiles.append(fullPath)
                 i += 1
+            SaveArray()
 
 
 def GetTextFromFile(path: str):
-    suffix=Path(path).suffix
-    if suffix==".pdf":
+    ppath = Path(path)
+    suffix = ppath.suffix
+    notSupported = [".sys", ".exe"]
+    # более 500 мб не проверять
+    if suffix in notSupported or ppath.stat().st_size > 524288000:
+        return None
+    if suffix == ".pdf":
         return PDFReader(path)
-    elif suffix in [".doc",".docx"]:
+    elif suffix in [".doc", ".docx"]:
         return DOCReader(path)
     else:
         try:
@@ -91,8 +108,8 @@ def GetTextFromFile(path: str):
                 text = f.read()
             return text
         except:
-            text=PDFReader(path)
-            if text==None:
+            text = PDFReader(path)
+            if text == None:
                 return DOCReader(path)
 
 
