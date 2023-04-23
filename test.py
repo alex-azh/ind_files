7fefecef1503c10b6601def35409887c5b7362fa
alex-azh <62564545+alex-azh@users.noreply.github.com>
2023-04-23 19:10:52 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,26 @@
-from memory_profiler import profile
+import datetime
+from sentence_transformers import SentenceTransformer
+from transformers import AutoModel, AutoTokenizer
+import faiss
+from pathlib import Path
+model_name='cointegrated/rubert-tiny'
 
-@profile
-def test():
-    print('aga')
-    a=2*2
-    b=a*2
+start=datetime.datetime.now()
 
-test()
+with open(Path(r"C:/Program Files\dotnet\packs\Microsoft.Android.Ref.33\33.0.4\ref\net7.0\Mono.Android.dll"), 'r',encoding="UTF-8") as f:
+    text=f.read()
+model = SentenceTransformer(model_name)
+m=model.encode([text])
+index = faiss.read_index("myindex2")
+res=index.search(m, 12)  # index.ntotal)
+print(res[0])
+print(res[1])
+# model=AutoModel.from_pretrained(model_name)
+# tk = AutoTokenizer.from_pretrained(model_name)
+# start=datetime.datetime.now()
+# encoded=tk(text,padding=True,truncation = True, return_tensors='pt')
+# output=model(**encoded)[1]
+# emd=output.detach().numpy()
+# end=datetime.datetime.now()
+# print(end-start)
+
